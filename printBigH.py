31dfb04e63f279371fdc361cd21d57fab9275941
TheFools <93574937+TheFools@users.noreply.github.com>
2021-11-09 15:40:38 +0800

    Update printBigH.py

--- printBigH.py
+++ printBigH.py
@@ -1,14 +1,14 @@
 print('''
-     H                     H            h
-     H                     H            h
-     H                     H            h
-     H                     H            h
-     H                     H            h
-     HHHHHHHHHHHHHHHHHHHHHHH            h  hhhhhhhh
-     H                     H            h h        h
-     H                     H            hh          h
-     H                     H            h           h
-     H                     H            h           h
-     H                     H            h           h
+     H                     H            
+     H                     H            
+     H                     H            
+     H                     H            
+     H                     H            
+     HHHHHHHHHHHHHHHHHHHHHHH            
+     H                     H            
+     H                     H            
+     H                     H            
+     H                     H
+     H                     H          
     
 ''')
